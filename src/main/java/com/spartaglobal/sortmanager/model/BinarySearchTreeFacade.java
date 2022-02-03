864e5ed08c8ff9c1f3c63d83d9d3322b37080102
Piotr Sulek <piotr.sulek@outlook.com>
2022-02-03 21:33:38 +0000

    Added more logs.

--- src/main/java/com/spartaglobal/sortmanager/model/BinarySearchTreeFacade.java
+++ src/main/java/com/spartaglobal/sortmanager/model/BinarySearchTreeFacade.java
@@ -1,11 +1,15 @@
 package com.spartaglobal.sortmanager.model;
 
+import com.spartaglobal.sortmanager.SortManagerMain;
+import com.spartaglobal.sortmanager.view.DisplayManager;
+
 import java.util.ArrayList;
 
 public class BinarySearchTreeFacade implements SortInterface{
 
     @Override
     public int[] sort(int[] ints) {
+        SortManagerMain.logger.debug("ints= " + new DisplayManager().displayArray(ints));
         if(ints.length == 0)
             return null;
 
@@ -15,6 +19,7 @@ public class BinarySearchTreeFacade implements SortInterface{
 
         for(int i = 0; i < ints.length; i++){
             bst.populateTree(root, ints[i]);
+            SortManagerMain.logger.debug("Entered value= " + ints[i]);
         }
 
         bst.traverseInOrder(root, list);
@@ -23,10 +28,11 @@ public class BinarySearchTreeFacade implements SortInterface{
 
         int i = 0;
         for(int value: list){
+            SortManagerMain.logger.debug("value= " + value);
             output[i] = value;
             i++;
         }
-
+        SortManagerMain.logger.debug("output= " + new DisplayManager().displayArray(output));
         return output;
     }
 }
